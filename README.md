b9f887ce9fc71fdfed4c241d6942ba16e7770cd5
func-boncey <thewarpdimension@gmail.com>
2014-12-13 21:18:25 +0000

    First Version
    First version of gemify.js.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ gemify.js
 =========
 
 A script to aid in the gemification of items.
+
+Just copy paste the stuff into your JS console in the browser. And tada, press the 'Gemify Multiple' button, make your selections by clicking on the items and then press 'Gemify Selections' and it will gemify them and refresh your browser.
+
+
+If anyone has any suggestions or would like to contribute, join me.
+
+
+Note: This was written very hackily and under a short time span due to demand by a friend, please fix any crappily written code if you have spotted some and have the time. Thanks. :)
---
+++ gemify.js
@@ -0,0 +1,112 @@
+var selected = [];
+var id = 0;
+
+var buttonHolder = document.createElement('div')
+buttonHolder.className = "flipthis-wrapper"
+
+var initButton = document.createElement('a');
+
+initButton.setAttribute("href", "javascript:init()")
+initButton.className = "btn_small btn_green_white_innerfade";
+
+iBDiv = document.createElement('div');
+iBDiv.className = "flipthis-wrapper"
+
+span = document.createElement('span')
+span.innerHTML = "Gemify Multiple"
+span.className = "";
+iBDiv.appendChild(span);
+
+initButton.appendChild(iBDiv);
+
+buttonHolder.appendChild(initButton)
+
+var row = document.getElementsByClassName("filter_ctn inventory_filters")
+row[0].appendChild(buttonHolder)
+
+function init()
+{
+	span.innerHTML = "Gemify Selections"
+	initButton.setAttribute("href", "javascript:gemifySelected()")
+
+	var buttons = document.getElementsByClassName("inventory_item_link")
+	for (var i = 0; i < buttons.length; i++)
+	{
+		var orig = buttons[i].getAttribute("href")
+		buttons[i].setAttribute("id", id)
+		buttons[i].setAttribute("onclick", "selectItem(\"" + orig + "\", " + id + ")")
+		id = id + 1;
+
+	}
+}
+
+function gemifySelected()
+{
+	for(var i = 0; i < selected.length; i++)
+	{
+		GrindIntoGooNoMess(267420, 6, selected[i].substring(7));
+		//console.log("GrindIntoGoo(267420, 6," + "\'" + selected[i].substring(7) + "\');")
+	}
+	location.reload();
+}
+
+function GrindIntoGooNoMess( appid, contextid, itemid )
+	{
+		var rgAJAXParams = {
+			sessionid: g_sessionID,
+			appid: appid,
+			assetid: itemid,
+			contextid: contextid
+		};
+		var strActionURL = g_strProfileURL + "/ajaxgetgoovalue/";
+
+		$J.get( strActionURL, rgAJAXParams ).done( function( data ) {
+			var $Content = $J(data.strHTML);
+			var strDialogTitle = data.strTitle;
+				strActionURL = g_strProfileURL + "/ajaxgrindintogoo/";
+				rgAJAXParams.goo_value_expected = data.goo_value;
+
+				$J.post( strActionURL, rgAJAXParams).done( function( data ) {
+				}).fail( function() {
+					ShowAlertDialog( strDialogTitle, 'There was an error communicating with the network. Please try again later.' );
+				});
+		});
+	}
+
+function selectItem(originalTag, id)
+{
+	var button = document.getElementById(id);
+	button.parentNode.parentNode.style.boxSizing = 'border-box';
+
+	if(originalTag.substring(0, 7) !=  "#753_6_")
+	{
+		return false;
+	}
+	for(var i = 0; i < selected.length; i++)
+	{
+		if(selected[i] == originalTag)
+		{
+			var index = selected.indexOf(i);
+			selected.splice(index, 1);
+			button.parentNode.parentNode.style.border = '';
+			return false;
+		}
+	}
+	selected.push(originalTag)
+	button.parentNode.parentNode.style.border = '2px dashed red'
+}
+
+function getAllElementsWithAttribute(attribute)
+{
+  var matchingElements = [];
+  var allElements = document.getElementsByTagName('*');
+  for (var i = 0, n = allElements.length; i < n; i++)
+  {
+    if (allElements[i].getAttribute(attribute))
+    {
+      // Element exists with attribute. Add to array.
+      matchingElements.push(allElements[i]);
+    }
+  }
+  return matchingElements;
+}
